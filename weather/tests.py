c226967b23e57d1d518047a341bce81c0a4c7f96
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-06-17 12:05:16 -0500

    Add simple test (#2)
    * Add files via upload
    
    * Update tests.py

--- weather/tests.py
+++ weather/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+HTTP_OK = 200
+
+
+class IndexPageTests(TestCase):
+
+    def test_index_page_status_code(self):
+        self.assertEqual(self.client.get('/').status_code, HTTP_OK)
