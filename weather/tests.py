0cfed80c71e3279f641b386b25821b0efd550291
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-06-17 12:01:42 -0500

    Update tests.py

--- weather/tests.py
+++ weather/tests.py
@@ -1,9 +1,9 @@
-from django.test import SimpleTestCase
+from django.test import TestCase
 
 HTTP_OK = 200
 
 
-class IndexPageTests(SimpleTestCase):
+class IndexPageTests(TestCase):
 
     def test_index_page_status_code(self):
         self.assertEqual(self.client.get('/').status_code, HTTP_OK)
