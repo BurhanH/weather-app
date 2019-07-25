05e54b9cf759e700ba8b832f6c901a68397d9637
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-07-24 19:53:52 -0500

    Update settings.py
    Fix

--- WeatherApp/settings.py
+++ WeatherApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g%(8*jo14#jt#qqz_hg2zoxd%f&)b@3i@x%9v^n79(y%*&-+i^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.0', 'aua-raiy.herokuapp.com']
 
